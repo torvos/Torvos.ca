df2fd70e74966cd65ad51aa91e864a8dea1fcce6
Torvos <torvos@torvos.ca>
2026-06-29 19:43:45 -0400

    missed a line

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
 window.startTorvosTerminal = function () {
-
     const terminal = new TerminalEngine(window.TorvosConfig);
-
     window.__TERMINAL__ = terminal;
-
     terminal.init();
-
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    const boot = new BootSequence(window.TorvosConfig);
-
+    const boot = new BootSequence(window.TorvosConfig, terminal);
     boot.start();
-
 });
