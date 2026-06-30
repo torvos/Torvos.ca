dc7767ca80d8bb631998721134d581e3c0966613
Trovos <philippet@gmail.com>
2026-06-29 20:46:06 -0400

    fix

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 window.startTorvosTerminal = function () {
     const terminal = new TerminalEngine(window.TorvosConfig);
     window.__TERMINAL__ = terminal;
-    const boot = new BootSequence(window.TorvosConfig, terminal);
     terminal.init();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     const boot = new BootSequence(window.TorvosConfig, terminal);
-    boot.start();
+    boot.finish();
+    //boot.start();
 });
