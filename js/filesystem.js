78da9ad7f0130f2eb4b15c9a37586036698fb683
Torvos <torvos@torvos.ca>
2026-07-07 23:00:15 -0400

    updated bio and contact info

--- js/filesystem.js
+++ js/filesystem.js
@@ -8,32 +8,43 @@
                 children: {
                     "bio.md": {
                         type: "file",
-                        content: `Bio comming soon, for now just look at linkedIn or find me on Slack/Discord.`
-                    }
+                        content: `
++------------------------------------------------------------------------------------------+
+| Senior cloud architecture and cybersecurity leader with more than 23 years of            |
+| experience delivering enterprise technology solutions. Recognized for leading cloud      | 
+| modernization initiatives, establishing enterprise architecture and security strategies, |
+| building high-performing technical teams, and advising executives on cloud adoption,     |
+| cybersecurity, DevSecOps, and digital transformation.                                    |
++------------------------------------------------------------------------------------------+
+`}
                 }
             },
 
-            blog: {
+/*            blog: {
                 type: "dir",
                 children: {
                     "welcome.md": {
                         type: "file",
-                        content: "Welcome... I'll write something at some point once I've finished this."
+                        content: 
+"Welcome... I'll write something at some point once I've finished this."
                     }
                 }
-            },
+            },*/
 
             "contact.md": {
                 type: "file",
-                content: `Email: contact@torvos.ca
-GitHub: https://github.com/torvos
-Bluesky: https://bsky.app/profile/torvos.bsky.social
-LinkedIn: https://www.linkedin.com/in/philippet/`
-            },
+                content: `
++--------------------------------------------------------+
+| - Email: contact@torvos.ca                             |
+| - GitHub: https://github.com/torvos                    |
+| - Bluesky: https://bsky.app/profile/torvos.bsky.social |
++--------------------------------------------------------+
+            `},
 
             "resume.md": {
                 type: "file",
-                content: "Resume download available soon, check linkedIn for now."
+                content: 
+"Resume download available soon, check linkedIn for now."
             }
 
         }
